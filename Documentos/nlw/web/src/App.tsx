45185f6117fb4d0d7bb003c2e50b5b54c30855d2
Lucas Borgonovi <lucas.lborgonovi@gmail.com>
2020-06-05 19:45:58 -0300

    Primeiro commit

---
+++ Documentos/nlw/web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Home from './pages/Home/index';
+
+import Routes from './routes';
+
+function App() {
+  return (
+
+      <Routes/>
+  );
+}
+
+export default App;
